62475ded1b7223444fd9a9892d4ff1fd8e669d37
Edgar <fogniebla@hotmail.com>
2020-11-18 09:55:28 -0500

    manejo errores

--- Spotiapp Angular/src/app/components/home/home.component.ts
+++ Spotiapp Angular/src/app/components/home/home.component.ts
@@ -10,12 +10,22 @@ import { SpotifyService } from '../../services/spotify.service';
 export class HomeComponent implements OnInit {
 
     nuevasCanciones: any[] = []
-    loading: boolean = true;
+    loading: boolean;
+    mensajeError: string;
+    error: boolean;
 
     constructor(private spotify: SpotifyService) {
+
+        this.loading= true;
+        this.error = false;
+
         this.spotify.getNewReleases().subscribe((res: any) => {
             this.nuevasCanciones = res
             this.loading = false;
+        }, (error_Servicio) => {
+                this.loading = false;
+                this.error = true;
+                this.mensajeError = error_Servicio.error.error.message;
         });
     }
 
