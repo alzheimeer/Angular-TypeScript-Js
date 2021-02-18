22e8fe3e68a8a9b1de1e5336f2fca9c7f9968611
Alzheimeer <fogniebla@hotmail.com>
2021-02-18 08:44:05 -0500

    new features

---
+++ 12 - HTTP - CRUD - Firebase - Angular/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 12 - HTTP - CRUD - Firebase - Angular/src/app/pages/heroe/heroe.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HeroeModel } from 'src/app/models/heroe.model';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styleUrls: ['./heroe.component.css']
+})
+export class HeroeComponent implements OnInit {
+
+
+  heroe: HeroeModel = new HeroeModel();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  guardar(form: NgForm){
+    console.log(form);
+    console.log(this.heroe);
+  }
+}
